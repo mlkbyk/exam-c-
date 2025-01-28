bb7ca32a84de920b08d69a4f03b4fa9a8b88e4ed
mlkbyk <166039308+mlkbyk@users.noreply.github.com>
2025-01-28 20:17:20 +0300

    Create question7.cpp

---
+++ 2013/question7.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class myclass {
+public:
+	double a;
+	double b;
+	myclass(double x, double y) {
+		a = x;
+		b = y;
+		cout << "Constructing object a= " << a << " b= " << b << endl;
+
+	}
+	~myclass() {
+		cout << "Destroying object a= " << a << " b= " << b << endl;
+
+	}
+	void show_ab() {
+		cout << "value of a= " << a << " b= " << b << endl;
+	}
+};
+myclass fun1(myclass* ob1) {
+	myclass ob2(4, 5);
+	ob2.a = ob2.a * ob1->a;
+	ob2.b = ob2.b - ob1->b;
+	return ob2;
+}
+
+int main() {
+	myclass obx(4, 6), oby(10, 20);
+	oby = fun1(&obx);
+	oby.show_ab();
+	return 0;
+}
